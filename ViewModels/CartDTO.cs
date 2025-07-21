7156c1b71c38f0ed3700357d98d7e7ac2a9b46fa
Kerim_Lex <lexmeins1@gmail.com>
2025-07-21 14:13:31 +0500

    Added DTO classes for cart and cart items
    Added `CartDTO.cs` class `CartDTO` class with necessary properties and static `AutoMapper` method for converting `Cart` object to `CartDTO`.
    
    In the `CartItemDTO.cs` file `CartItemDTO` class is added containing properties for cart ID, quantity, animal ID and creation date.

---
+++ ViewModels/CartDTO.cs
@@ -0,0 +1,32 @@
+﻿using TameShop.Models;
+
+namespace TameShop.ViewModels
+{
+    public class CartDTO
+    {
+        public required string CartId { get; set; }
+        public required string UserId { get; set; }
+        public List<CartItemDTO>? Items { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+        public static CartDTO AutoMapper(Cart cart)
+        {
+            var cartDto = new CartDTO
+            {
+                CartId = cart.CartId,
+                UserId = cart.UserId,
+                Items = cart.Items.Select(i => new CartItemDTO
+                {
+                    CartId = i.CartId,
+                    Quantity = i.Quantity,
+                    AnimalId = i.AnimalId
+                }).ToList(),
+                CreatedAt = cart.CreatedAt,
+                UpdatedAt = cart.UpdatedAt
+            };
+
+            return cartDto;
+        }
+    }
+}
---
+++ ViewModels/CartItemDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace TameShop.ViewModels
+{
+    public class CartItemDTO
+    {
+        public string CartId { get; set; } = string.Empty;
+        public int Quantity { get; set; }
+        public int AnimalId { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+    }
+}
